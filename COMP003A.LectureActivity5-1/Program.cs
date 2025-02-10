33b77aa3fb29f22f56a3a4a9e45d1d9872df25d2
SalinasJason <jasonsalinas770@gmail.com>
2025-02-10 15:28:15 -0800

    Added Step 4: Implement a Constructor and Step 5: Add Properties for Encapsulation

--- COMP003A.LectureActivity5-1/Program.cs
+++ COMP003A.LectureActivity5-1/Program.cs
@@ -3,14 +3,43 @@
     internal class Car
     {
         // Fields
-        public string Make;
-        public string Model;
-        public int Year;
+        private string make;
+        private string model;
+        private int year;
+
+        // Properties
+        public string Make
+        {
+            get { return make; }
+            set { make = value; }
+        }
+        public string Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+        public int Year
+        {
+            get { return year; }
+            set { if (value >= 1886) year = value; }
+        }
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        /// <param name="make"></param>
+        /// <param name="model"></param>
+        /// <param name="year"></param>
+        public Car(string make, string model, int year) 
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+        }
 
         /// <summary>
         /// Display the car information
         /// </summary>
-        /// 
         public void DisplayInfor()
         {
             Console.WriteLine($"Car: {Year} {Make} {Model}");
@@ -20,12 +49,13 @@
     {
         static void Main(string[] args)
         {
-            Car myCar = new Car(); // Create a new Car
-            myCar.Make = "Toyota"; // Set the car's make
-            myCar.Model = "Supra"; // Set the car's model
-            myCar.Year = 2024; // Set the car's year
-
+            Car myCar = new Car("Honda", "Civic", 2021); // Create a new Car
             myCar.DisplayInfor(); // Display the car's information
+
+            myCar.Make = "Toyota"; // Change the make of the car
+            myCar.Model = "Corolla"; // Change the model of the car
+            myCar.Year = 2020; // Change the year of the car
+            myCar.DisplayInfor();
         }
     }
 
