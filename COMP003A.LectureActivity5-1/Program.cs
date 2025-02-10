ef725ed8fe249bd6237ebe3aa4254486c7a46494
SalinasJason <jasonsalinas770@gmail.com>
2025-02-10 15:10:09 -0800

    Added Step 2: Define the Class and Step 3: Instantiate and Use the Class

--- COMP003A.LectureActivity5-1/Program.cs
+++ COMP003A.LectureActivity5-1/Program.cs
@@ -1,10 +1,33 @@
 ﻿namespace COMP003A.LectureActivity5_1
 {
+    internal class Car
+    {
+        // Fields
+        public string Make;
+        public string Model;
+        public int Year;
+
+        /// <summary>
+        /// Display the car information
+        /// </summary>
+        /// 
+        public void DisplayInfor()
+        {
+            Console.WriteLine($"Car: {Year} {Make} {Model}");
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Car myCar = new Car(); // Create a new Car
+            myCar.Make = "Toyota"; // Set the car's make
+            myCar.Model = "Supra"; // Set the car's model
+            myCar.Year = 2024; // Set the car's year
+
+            myCar.DisplayInfor(); // Display the car's information
         }
     }
+
+
 }
